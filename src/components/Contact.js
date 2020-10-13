3bdf277295a2663c9c08df77bc79cb4f157989ec
christopheveloupoule <christopheveloupoule@yahoo.fr>
2020-10-13 16:22:28 +0200

    Quest about REACT first project

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Contact.css';
+
+function Contact() {
+  return (
+    <div className="Contact">
+      <a href="https://randomuser.me/api/portraits/women/80.jpg">Portrait</a>
+    </div>
+  );
+}
+
+export default Contact;
