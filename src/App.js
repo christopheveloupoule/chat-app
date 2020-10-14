14d859208eee2e2d9012a566c991e84f69d65a6b
christopheveloupoule <christopheveloupoule@yahoo.fr>
2020-10-14 15:57:15 +0200

    Contact

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import Contact from './components/Contact.js';
+import React from "react";
+import Contact from "./components/Contact.js";
+import "./components/Contact.css";
 
 function App() {
   return (
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import './Contact.css';
+import React from "react";
+import "./Contact.css";
+
+const nom = "Jimmy Shaw";
+const statut = true;
+const avatar = (
+  <img
+    className="avatar"
+    src="https://randomuser.me/api/portraits/men/74.jpg"
+  />
+);
 
 function Contact() {
   return (
     <div className="Contact">
-      <a href="https://randomuser.me/api/portraits/women/80.jpg">Portrait</a>
+      <div>{avatar}</div>
+      <div>
+        <div className="name">{nom}</div>
+        <div className="status">
+          <p className="status-text">
+            <div class="status-online"></div>
+            {statut ? "online" : "offline"}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
