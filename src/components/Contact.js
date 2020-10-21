5147ba860bdede81384deebbd0d70e75a180d1fd
christopheveloupoule <christopheveloupoule@yahoo.fr>
2020-10-21 19:45:51 +0200

    Test React State

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,28 +1,31 @@
 import React from "react";
 import './Contact.css';
-import PropTypes from "prop-types";
 
-/*props*/
 
-function Contact(props) {
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: false,
+    };
+  
+  
+  render() {
   return (
     <div className="Contact">
-      <img className="avatar" src={props.avatar} alt={props.name} />
+      <img className="avatar" src={this.props.avatar} alt={this.props.name} />
       <div>
-        <h3 className="name">{props.name}</h3>
+        <h3 className="name">{this.props.name}</h3>
         <div className="status">
-          <div className={props.online ? "status-online" : "status-offline"} />
-          <p className="status-text">{props.online ? "Online" : "Offline"}</p>
+          <div className={this.props.online ? "status-online" : "status-offline"} />
+          <p className="status-text">{this.props.online ? "Online" : "Offline"}</p>
         </div>
       </div>
     </div>
   );
 }
 
-Contact.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  status: PropTypes.bool.isRequired
-};
+
 
 export default Contact;
